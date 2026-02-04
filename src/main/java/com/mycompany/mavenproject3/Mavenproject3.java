3e226f3396781ac1260cbc7418b7cdb2144a0cc0
armirani97-coder <armirani97@gmail.com>
2026-02-04 22:12:21 +0000

    Update Mavenproject3.java

--- src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
+++ src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -4,8 +4,6 @@
 
 package com.mycompany.mavenproject3;
 
-import java.util.Scanner;
-
 /**
  *
  * @author armir
@@ -33,18 +31,19 @@ public class Mavenproject3 {
         months[11]="December";
         float[] temprature2023= new float[12];
         float[] temprature2024=new float[12];
-        //-----------2023 temprature input-------
+        //-----------2023 temprature input-------//
         System.out.println("Enter temprature for the months of 2023:");
         for(int i=0;i<12;i++){
         System.out.print(months[i]+"=");
         temprature2023[i]=s.nextFloat();}
         
         
-        //--------2024 temprature iput 2024----
+        //--------2024 temprature iput 2024----//
         System.out.println("Enter temprature for the months of 2024:");
         for(int i=0;i<12;i++){
         System.out.print(months[i]+"=");
         temprature2024[i]=s.nextFloat();}
+        //-----Temprature display for 2023 and 2024----//
         
         System.out.println("\ntemprature for the months of 2023:");
         for(int i=0;i<12;i++)
@@ -54,7 +53,15 @@ public class Mavenproject3 {
         for(int i=0;i<12;i++){
         System.out.println(months[i]+"="+temprature2024[i]+"Celcius");
         }
-        
+        //------- True or false if the data is equal-----//
+        //------Boolean true or false------//
+        boolean dataisidentical=true;
+        for(int i=0;i<12;i++){
+        if(temprature2023[i] != temprature2024[i])
+        {dataisidentical=false;}
+        } System.out.println("Do the year 2023 and 2024 have the same data?");
+        System.out.println("The answer is\t" + dataisidentical);
+      
         
         
     }
